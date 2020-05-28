ffad3663fb1f61a1074d8f1184a8dfd16897c20f
Emmanuel Ezenwigbo <44367769+SkyC0der@users.noreply.github.com>
2020-05-28 12:37:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const square = () => {
+  throw new Error("Remove this statement and implement this function");
+};
+
+const total = () => {
+  throw new Error("Remove this statement and implement this function");
+};
+
+
+module.exports = {square, total}
